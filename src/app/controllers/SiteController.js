65dcd1bc124a57e93e38580fbd5867eddca2d5b4
Lê Quý Độ <leedo.devwork@gmail.com>
2024-04-21 22:07:06 +0700

    Connect to mongodb

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,16 @@
+const Course = require('../models/Courses')
+
 class NewsController {
   //[GET] /site
-  index(req, res) {
-    res.render('home');
+  async index(req, res) {
+    try {
+      const data = await Course.find({})
+      res.json(data)
+    } catch (err) {
+      res.status(400).json({ error: err })
+    }
+
+    // res.render('home');
   }
 
   //[GET] /site/search
--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
+
 const route = require('./routes');
+const db = require('./config/db');
+
+db.connect();
 
 //static file
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,11 +27,11 @@ app.use(morgan('combined'));
 //Template engine
 app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resource/views'));
+app.set('views', path.join(__dirname, 'resource', 'views'));
 
 //Routes init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
