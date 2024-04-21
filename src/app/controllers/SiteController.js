2fba43e1761ae327173937f371e08797ad21202a
Lê Quý Độ <leedo.devwork@gmail.com>
2024-04-21 18:42:06 +0700

    install prettier lint-staged husky

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,13 @@
+class NewsController {
+  //[GET] /site
+  index(req, res) {
+    res.render('home');
+  }
+
+  //[GET] /site/search
+  search(req, res) {
+    res.render('search');
+  }
+}
+
+module.exports = new NewsController();
