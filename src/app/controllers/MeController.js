0c702b37ddac5793e1244620c1f3c7eacbbabda2
Lê Quý Độ <leedo.devwork@gmail.com>
2024-05-08 21:23:40 +0700

    Sort middleware

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -6,8 +6,16 @@ const { multipleMongooseToObject } = require("../../util/mongoose");
 class MeController {
   //[GET] me/stored/courses
   storedCourses(req, res, next) {
+    let courseQuery = Course.find({})
+
+    if (req.query.hasOwnProperty('_sort')) {
+      courseQuery = courseQuery.sort({
+        [req.query.column]: req.query.type
+      })
+    }
+
     //Count with asynchronous
-    Promise.all([Course.find({}), Course.countDocumentsWithDeleted({ deleted: true })])
+    Promise.all([courseQuery, Course.countDocumentsWithDeleted({ deleted: true })])
       .then(([courses, deletedCount]) =>
         res.render("me/stored-coures", { deletedCount, courses: multipleMongooseToObject(courses) })
       )
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const { engine } = require("express-handlebars");
 const methodOverride = require("method-override");
 
+const SortMiddleware = require('./app/middlewares/SortMiddleware')
+
 const route = require("./routes");
 const db = require("./config/db");
 
@@ -26,6 +28,9 @@ app.use(express.json());
 //Override using a header
 app.use(methodOverride("_method"));
 
+//Custome middleware
+app.use(SortMiddleware)
+
 //HTTP logger
 app.use(morgan("combined"));
 
@@ -37,6 +42,28 @@ app.engine(
     //Custom helpers:
     helpers: {
       sum: (a, b) => a + b,
+      sortable: (field, sort) => {
+        const sortType = field === sort.column ? sort.type : 'default'
+
+        const icons = {
+          default: 'fa-solid fa-sort',
+          asc: 'fa-solid fa-arrow-down-short-wide',
+          desc: 'fa-solid fa-arrow-down-wide-short'
+        }
+        const types = {
+          default: 'desc',
+          asc: 'desc',
+          desc: 'asc'
+        }
+
+        const icon = icons[sortType]
+        const type = types[sortType]
+
+        return `<a href="?_sort&column=${field}&type=${type}">
+        <i class="${icon}"></i>
+      </a>`
+
+      }
     },
   })
 );
